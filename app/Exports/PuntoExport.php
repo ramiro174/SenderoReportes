86c7f7463cf170b72abc2c94d4d5271864b692a1
Ramiro <ramiro174@gmail.com>
2023-07-06 18:08:08 -0600

    ad punto

--- app/Exports/PuntoExport.php
+++ app/Exports/PuntoExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Punto;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PuntoExport implements FromCollection
+class PuntoExport implements FromCollection,WithHeadingRow
 {
     /**
     * @return \Illuminate\Support\Collection
--- app/Http/Controllers/SenderoController.php
+++ app/Http/Controllers/SenderoController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Exports\PuntoExport;
-use App\Models\Punto;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
